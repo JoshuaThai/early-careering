40a6fb85cef2d9969b514f9b7bee2b1086387d4f
JoshuaThai <joshua.thaisananikone@gmail.com>
2026-08-20 17:02:27 -0500

    Added bare-bones homepage

--- early-careering/src/app/page.tsx
+++ early-careering/src/app/page.tsx
@@ -1,70 +1,42 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars} from "@fortawesome/free-solid-svg-icons";
+
+
+function Title() {
+    return (
+        <div className={styles.title}>
+            <h1 className={styles.titleText}>EarlyCareering</h1>
+            <p className={styles.subtitle}>Your career starts here! Track your progress and achieve your goals.</p>
+        </div>
+    )
+};
+
+function Header(){
+    return (
+        <div className={styles.header}>
+            <h1 className={styles.logo}><a href="/">EarlyCareering</a></h1>
+            <div className={styles.navContainer}>
+                <button className={styles.menuButton} aria-label="Menu">
+                    <FontAwesomeIcon icon={faBars} 
+                    className={styles.menuIcon} 
+                    size="lg"/>
+                </button>
+                <nav className={styles.nav}>
+                    <a href="/">Home</a>
+                    <a href="/somePage">Features</a>
+                    <a href="/somePage">About</a>
+                </nav>
+                <button className={styles.loginButton}>Login/ Sign Up</button>
+            </div>
+        </div>
+)};
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>
-            To get started, edit the{" "}
-            <code className={styles.code}>page.tsx</code> file.
-          </h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-        <p>YES!</p>
-      </main>
+      <Header />
+      <Title />
     </div>
-  );
-}
+)};
