b0acde627f1aa21129d66849de11b970fb2adb3e
JoshuaThai <145885143+JoshuaThai@users.noreply.github.com>
2026-09-10 00:37:38 -0500

    Debugged the database

--- early-careering/src/db/db.ts
+++ early-careering/src/db/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import * as schema from "./auth-schema";
 
-const connection = await mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
